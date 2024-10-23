68976a462373642985f9da78621158818c21b28c
besaoct <blackipie@outlook.com>
2024-10-23 22:46:24 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -17,10 +17,25 @@ const nextConfig = {
             has: [
               {
                 type: 'host',
-                value: ':username.boxmarkdown.com'
+                value: 'boxmarkdown.com'
               }
             ],
-            destination: 'https://:username.boxmarkdown.com/:username/:projectSlug/:slug',
+            destination: 'https://boxmarkdown.com/:username/:projectSlug/:slug',
+          },
+        ];
+      },
+
+      async redirects() {
+        return [
+          {
+            source: '/:username/:projectSlug/:slug',
+            has: [
+              {
+                type: 'host',
+                value: 'boxmarkdown.com'
+              }
+            ],
+            destination: 'https://:username.boxmarkdown.com/:projectSlug/:slug',
           },
         ];
       }
