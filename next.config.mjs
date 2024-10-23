a5390c71b3c95895ca78fecd6c6750a3fa6474da
besaoct <blackipie@outlook.com>
2024-10-24 00:16:46 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -16,49 +16,19 @@ const nextConfig = {
       async rewrites() {
         return [
           {
-            source: '/legal/privacy-policy',  // This is the path on the subdomain
+            source: '/:projectSlug/:slug',  // This is the path on the subdomain
             has: [
               {
                 type: 'host',
-                value: 'main.boxmarkdown.com',  // This matches the 'main' subdomain
+                value: ':username.boxmarkdown.com',  // This matches the 'main' subdomain
               },
             ],
-            destination: 'https://boxmarkdown.com/main/legal/privacy-policy',  // Redirect to the main site
+            destination: 'https://boxmarkdown.com/:username/:projectSlug/:slug',  // Redirect to the main site
           },
         ];
       },
 
-      async redirects() {
-        return [
-            {
-                source: '/main/legal/privacy-policy',  // This is the path on the subdomain
-                has: [
-                    {
-                        type: 'host',
-                        value: 'boxmarkdown.com',  // This matches the root domain
-                    },
-                ],
-                destination: 'https://main.boxmarkdown.com/legal/privacy-policy',  // Redirect to the main site
-                permanent: true,
-            },
-            {
-                source: '/main/legal/privacy-policy',  // This is the same path
-                has: [
-                    {
-                        type: 'host',
-                        value: 'www.boxmarkdown.com',  // This matches the www subdomain if needed
-                    },
-                ],
-                destination: 'https://main.boxmarkdown.com/legal/privacy-policy',  // Redirect to the main site
-                permanent: true,
-            },
-            {
-                source: '/main/legal/privacy-policy',  // Redirect from the root domain
-                destination: 'https://main.boxmarkdown.com/legal/privacy-policy',  // Redirect to the main site
-                permanent: true,
-            },
-        ];
-    },
+  
     
 };
 
