b25349262c28a3e8c7adcfefc0bf54eaef0a4ea1
besaoct <blackipie@outlook.com>
2024-10-23 21:12:49 +0530

    fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import Link from "next/link"
 
-const Notfound = () => {
+ 
+export default function NotFound() {
   return (
-    <div className='min-h-screen h-full flex flex-col justify-center items-center w-full'>
-       Page not found
-    </div>
+
+<div className="flex gap-2 flex-col items-center justify-center h-screen w-full text-center mx-auto p-4">
+  <h2 className="text-xl font-bold text-red-600">Oops! <p className='inline text-foreground text-lg font-normal'>looks like we missed the mark! 🔥</p></h2>
+  <p className="text-lg">Not quite what you were craving? No worries!</p>
+  <Link href={''} className='underline text-indigo-500 font-semibold hover:text-indigo-700'>Give it another shot!</Link>
+  <span className="mx-1">or</span>
+  <Link href={'/'} className='underline text-indigo-500 font-semibold hover:text-indigo-700'>Get back to your happy place! 🏡</Link>
+</div>
+
   )
 }
-
-export default Notfound
