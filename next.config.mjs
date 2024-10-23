17e626621ec51e576bc21e2a3442c528f6d99d4c
besaoct <blackipie@outlook.com>
2024-10-23 23:25:36 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
+  async rewrites() {
+    return [
+      {
+        source: '/:projectSlug/:slug',  // Handle subdomain routing path
+        has: [
+          {
+            type: 'host',
+            value: ':username.boxmarkdown.com',  // Match subdomain
+          },
+        ],
+        destination: '/:username/:projectSlug/:slug',  // Route to dynamic folder structure
+      },
+    ];
+  },
+
+
     images: {
         remotePatterns: [
           {
@@ -8,10 +25,7 @@ const nextConfig = {
             port: '',
           },
         ],
-      },
-
-
-
+      }
       
 };
 
