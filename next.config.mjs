a36b2a0b46f1b6353100026711aa6018b534bf02
besaoct <blackipie@outlook.com>
2024-10-24 00:57:46 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -13,32 +13,6 @@ const nextConfig = {
         ],
       },
 
-      async rewrites() {
-        return [
-            {
-                source: '/:projectSlug/:slug',  // This is the path on the subdomain
-                has: [
-                    {
-                        type: 'host',
-                        value: 'boxmarkdown.com',  // This matches the root domain
-                    },
-                ],
-                destination: 'https://boxmarkdown.com/:username/:projectSlug/:slug',  // Redirect to the main site
-            },
-            {
-                source: '/:projectSlug/:slug',  // This is the path for dynamic subdomains
-                destination: 'https://boxmarkdown.com/:username/:projectSlug/:slug',  // Redirect to the main site
-                has: [
-                    {
-                        type: 'host',
-                        value: '(.*).boxmarkdown.com',  // Regular expression for dynamic subdomains
-                    },
-                ],
-            },
-        ];
-    },
-
-  
     
 };
 
